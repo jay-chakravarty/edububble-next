6bd3a90b2966b578b4b1fefd9dcc9de4521f4414
Jay Chakravarty <jaychakravarty@icloud.com>
2026-07-04 10:18:41 -0700

    add topics route

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center p-4 space-y-2 border border-black bg-crimson font-chalkduster text-white rounded-xl">
+    <footer className="text-center p-4 space-y-2 border border-black bg-crimson text-white rounded-xl">
+      <h1>
+        <Link href="/topics">
+          <span className="text-blue-500">Study Topics</span>
+        </Link>
+      </h1>
       <h1>Next.js + Firebase</h1>
       <div className="flex justify-center space-x-2">
         <Image
